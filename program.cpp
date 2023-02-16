16e088decd653a08bea05429c08681681b385351
FRIDRICHOVSKY Jan <fridrichovsky@gmail.com>
2023-02-16 20:38:08 +0100

    The build application added

--- program.cpp
+++ program.cpp
@@ -1,7 +1,13 @@
-#include "stdio.h"
+#include <iostream>
 
 int main()
 {
+    auto autoVariable = 20;
+    decltype(autoVariable) decTypeVariable = 21;
+
+    std::cout << "First variable " << autoVariable << std::endl;
+    std::cout << "Second variable " << decTypeVariable << std::endl;
+
     printf("C++ Standards application\n");
     return 0;
 }
