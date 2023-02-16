9b29bcb5b4fae06f9de5c938e2ce334fb9658f91
FRIDRICHOVSKY Jan <fridrichovsky@gmail.com>
2023-02-16 21:57:23 +0100

    The function declaration has been added

--- program.cpp
+++ program.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
+#include <vector>
+
+class A
+{
+    int x;
+
+    A();
+    A(A& other)
+    {
+        this->x = other.x;
+    }
+};
+
+class B: A
+{
+
+};
+
+[[pnoreturn]] static const int functionVoid()
+{
+    std::cout << "this is the call of the function" << std::endl;
+    return 1;
+}
 
 int main()
 {
@@ -8,6 +31,15 @@ int main()
     std::cout << "First variable " << autoVariable << std::endl;
     std::cout << "Second variable " << decTypeVariable << std::endl;
 
+    std::vector<int> arrayVariable = {1,4,5,6,7};
+
+    for (auto value: arrayVariable)
+    {
+        std::cout << "value: " << value << std::endl;
+    }
+
+    autoVariable = functionVoid();
+    std::cout << "functoin has returned" << autoVariable << std::endl;
     printf("C++ Standards application\n");
     return 0;
 }
