1aa7028e33dc32317487c58d0721c5a91cb532e7
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2023-04-26 17:34:25 +0200

    Added uniform initializer order example

--- Cpp11_UniformInitialization.cpp
+++ Cpp11_UniformInitialization.cpp
@@ -1,9 +1,22 @@
 #include "Cpp11_UniformInitialization.hpp"
 #include <iostream>
+#include <initializer_list>
 
 struct SomeStructure{
     int m_id;
-    std::string m_name; 
+    std::string m_name;
+};
+
+struct SomeOrderExample{
+    int m_value;                // 1st choice
+    SomeOrderExample(int value){ // 2nd choice
+        m_value = value;
+    } 
+    SomeOrderExample(const std::initializer_list<int>& values){ // 1st choice
+        for (auto value:values){
+            std::cout << "Item:" << value << std::endl;
+        }
+    }
 };
 
 // C++ 11 - extended the initialization options to the curly brace list also for the internal variables
@@ -16,4 +29,8 @@ void Cpp11_UniformInitialization::uniformInit_SimpleExample()
 {
     SomeStructure somStr{1, "SomeName"};
     std::cout << "name id: " << somStr.m_name << std::endl;
+    
+    // example of the order
+
+
 };
